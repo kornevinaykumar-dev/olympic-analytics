06b4912cbc5d1d17ae1a2dc88410d09124ac9460
vinay kumar korne <vinay@vinay.Example.com>
2026-06-09 23:24:21 +0530

    Render deployment fix

--- Backend/app.py
+++ Backend/app.py
@@ -52,7 +52,8 @@ def create_app():
 
     return app
 
+# Create app for Gunicorn/Render
+app = create_app()
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=True)
